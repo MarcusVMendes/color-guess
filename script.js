d9e3b01bae2037ff8678b896909d2b8cb6ae2046
MarcusVMendes <marcus.mendes89@gmail.com>
2021-12-10 18:55:05 -0300

    implementa cores randomicas

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-const teste = document.querySelector('#title');
 
-teste.innerHTML = 'testado';
+
+function randomNumber() {
+  const number = Math.round(Math.random() * 255);
+
+  return number;
+}
+
+function colorCircle() {
+  const color1 = randomNumber();
+  const color2 = randomNumber();
+  const color3 = randomNumber();
+  const color = `rgb(${color1}, ${color2}, ${color3})`;
+
+  return color;
+}
+
+function createCircle() {
+  const circleList = document.querySelector('#circle-list');
+  const newCircle = document.createElement('div');
+  newCircle.className = 'ball';
+  newCircle.style.background = colorCircle();
+
+  circleList.appendChild(newCircle);
+}
+
+function apendCircle(qt) {
+  for (let index = 0; index < qt; index += 1) {
+    createCircle();
+  }
+}
+
+apendCircle(6);
